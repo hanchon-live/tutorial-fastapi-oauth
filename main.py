455a2bebacdd9b99a989351b3ae7f26b10c22481
Guillermo Paoletti <guillermo.paoletti@gmail.com>
2021-04-22 21:33:37 +0200

    Fix typo

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app.mount('/api', api_app)
 
 @app.get('/')
 async def root():
-    return HTMLResponse('<body><a href="/auth/login">Log In</button></body>')
+    return HTMLResponse('<body><a href="/auth/login">Log In</a></body>')
 
 
 @app.get('/logout')
